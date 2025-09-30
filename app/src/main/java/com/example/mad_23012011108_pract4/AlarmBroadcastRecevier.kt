c14f6915eec1630f67bc9df87132fea6faf5ded9
chi-ru28 <patelruchi2830@gmail.com>
2025-09-30 09:24:46 +0530

    practical 4

---
+++ app/src/main/java/com/example/mad_23012011108_pract4/AlarmBroadcastRecevier.kt
@@ -0,0 +1,22 @@
+package com.example.mad_23012011108_pract4
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmBroadCastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val str1 = intent.getStringExtra("Service1")
+        if(str1 == "Start" || str1 == "Stop"){
+            val intentService = Intent(context, AlarmService::class.java)
+            //intentService.putExtra("Service1", intent.getStringExtra("Service1"))
+            if(str1 == "Start")
+                context.startService(intentService)
+            else if(str1 == "Stop")
+                context.stopService(intentService)
+        }
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("AlarmBroadCastReceiver.onReceive() is not implemented")
+        }
+}
